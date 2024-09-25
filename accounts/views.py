eb88287aa5f238e4c2e2d0ab6f340f6bb651b9b0
Chuen <hcpoon@gmail.com>
2024-09-25 17:03:33 +0800

    comment code before uploading

--- accounts/views.py
+++ accounts/views.py
@@ -9,12 +9,12 @@ def login(request):
     # new_user.set_password("1234")
     # new_user.save()
 
-    new_rule = Rule(rule_name="rule6")
-    new_rule.fine_per_day = 1.5
-    new_rule.borrow_limit = 20
-    new_rule.reserve_limit = 20
-    new_rule.borrow_period =200
-    new_rule.save()
+    # new_rule = Rule(rule_name="rule6")
+    # new_rule.fine_per_day = 1.5
+    # new_rule.borrow_limit = 20
+    # new_rule.reserve_limit = 20
+    # new_rule.borrow_period =200
+    # new_rule.save()
     return render(request, 'accounts/login.html')
 
 
@@ -27,27 +27,27 @@ def dashboard(request):
 
 
 def register(request):
-    allrules_queryset = Rule.objects.all()
-    allusers_queryset = CustomUser.objects.all()
+    # allrules_queryset = Rule.objects.all()
+    # allusers_queryset = CustomUser.objects.all()
 
-    filterrule_queryset = Rule.objects.filter(borrow_limit=14)
-    selected_rule = Rule.objects.get(rule_name="default")
-    filteruser_queryset = CustomUser.objects.filter(rule=selected_rule)
-    length = len(filteruser_queryset)
-    print("number of user using this rule is " + str(length))
-    
+    # filterrule_queryset = Rule.objects.filter(borrow_limit=14)
+    # selected_rule = Rule.objects.get(rule_name="default")
+    # filteruser_queryset = CustomUser.objects.filter(rule=selected_rule)
+    # length = len(filteruser_queryset)
+    # print("number of user using this rule is " + str(length))
+
     # change borrow_limit of rule2 = 18
-    select_rule5 = Rule.objects.get(rule_name="rule5", fine_per_day=1.5)
-    select_rule5.rule_name = "rule 7"
-    select_rule5.save()
-    
-    context = {'myvariable': 3, 
-               'allrules_queryset': allrules_queryset,
-               'allusers_queryset': allusers_queryset,
-               'filterrule_queryset': filterrule_queryset,
-               'filteruser_queryset': filteruser_queryset,
-               }
-    return render(request, 'accounts/register.html', context)
+    # select_rule5 = Rule.objects.get(rule_name="rule5", fine_per_day=1.5)
+    # select_rule5.rule_name = "rule 7"
+    # select_rule5.save()
+
+    # context = {'myvariable': 3,
+    #         'allrules_queryset': allrules_queryset,
+    #          'allusers_queryset': allusers_queryset,
+    #          'filterrule_queryset': filterrule_queryset,
+    #          'filteruser_queryset': filteruser_queryset,
+    #           }
+    return render(request, 'accounts/register.html')
 
 
 def forgotpass(request):
